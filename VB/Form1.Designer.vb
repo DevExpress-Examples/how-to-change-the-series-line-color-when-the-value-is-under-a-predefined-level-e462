89ef7fe0ef4b096aed4e879639f38f2b22f76f2d
DevExpressExampleBot <fakeEmail>
2019-05-24 13:11:07 +0300

    Source auto update [skip ci]

---
+++ VB/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿Namespace ChartLineColor
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Dim lineSeriesView2 As New DevExpress.XtraCharts.LineSeriesView()
+			Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+			Me.button1 = New System.Windows.Forms.Button()
+			Me.textBox1 = New System.Windows.Forms.TextBox()
+			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(lineSeriesView2, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' chartControl1
+			' 
+			Me.chartControl1.Anchor = (CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+			Me.chartControl1.Legend.Name = "Default Legend"
+			Me.chartControl1.Location = New System.Drawing.Point(12, 12)
+			Me.chartControl1.Name = "chartControl1"
+			Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(){}
+			Me.chartControl1.SeriesTemplate.SeriesColorizer = Nothing
+			Me.chartControl1.SeriesTemplate.View = lineSeriesView2
+			Me.chartControl1.Size = New System.Drawing.Size(600, 258)
+			Me.chartControl1.TabIndex = 0
+			' 
+			' button1
+			' 
+			Me.button1.Location = New System.Drawing.Point(157, 286)
+			Me.button1.Name = "button1"
+			Me.button1.Size = New System.Drawing.Size(170, 20)
+			Me.button1.TabIndex = 1
+			Me.button1.Text = "Set level"
+			Me.button1.UseVisualStyleBackColor = True
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.button1.Click += new System.EventHandler(this.button1_Click);
+			' 
+			' textBox1
+			' 
+			Me.textBox1.Location = New System.Drawing.Point(333, 286)
+			Me.textBox1.Name = "textBox1"
+			Me.textBox1.Size = New System.Drawing.Size(118, 20)
+			Me.textBox1.TabIndex = 2
+			Me.textBox1.Text = "0"
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(624, 314)
+			Me.Controls.Add(Me.textBox1)
+			Me.Controls.Add(Me.button1)
+			Me.Controls.Add(Me.chartControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.Load += new System.EventHandler(this.Form1_Load);
+			CType(lineSeriesView2, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+			Me.PerformLayout()
+
+		End Sub
+
+		#End Region
+
+		Private chartControl1 As DevExpress.XtraCharts.ChartControl
+		Private WithEvents button1 As System.Windows.Forms.Button
+		Private textBox1 As System.Windows.Forms.TextBox
+	End Class
+End Namespace
+
---
+++ VB/Form1.vb
@@ -0,0 +1,96 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraCharts
+
+Namespace ChartLineColor
+	Partial Public Class Form1
+		Inherits Form
+
+		Private level As Double = 0
+		Private maxPointValue As Double = 0
+		Private minPointValue As Double = 0
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+			AddHandler chartControl1.BoundDataChanged, AddressOf OnBoundDataChanged
+			InitializeSeries()
+			ApplyColorizer()
+		End Sub
+		Public Sub InitializeSeries()
+			chartControl1.Series.Clear()
+
+			Dim series As New Series()
+			series.BindToData(PointGenerator.Generate(), "X", "Y")
+			series.ColorDataMember = "Y"
+			chartControl1.Series.Add(series)
+			series.LabelsVisibility = DevExpress.Utils.DefaultBoolean.True
+			series.Label.TextPattern = "{V:N2}"
+			TryCast(chartControl1.Diagram, XYDiagram).AxisX.Alignment = AxisAlignment.Zero
+		End Sub
+		Public Sub ApplyColorizer()
+			level = Convert.ToDouble(textBox1.Text)
+			Dim series As Series = chartControl1.Series(0)
+			Dim view As New StepLineSeriesView()
+			series.View = view
+			Dim colorizer As New RangeSegmentColorizer()
+			colorizer.RangeStops.AddRange(New Double() { minPointValue, level, maxPointValue })
+			colorizer.LegendItemPattern = "{V1:0.###} - {V2:0.###}"
+			view.SegmentColorizer = colorizer
+		End Sub
+
+		Public Sub OnBoundDataChanged(ByVal sender As Object, ByVal e As EventArgs)
+			Dim series As Series = chartControl1.Series(0)
+			minPointValue = series.Points.Min(Function(p) p.UserValues(0))
+			maxPointValue = series.Points.Max(Function(p) p.UserValues(0))
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			InitializeSeries()
+			ApplyColorizer()
+		End Sub
+	End Class
+
+	Friend Class PointGenerator
+		Public Shared Function Generate() As List(Of SimpleDataPoint)
+			Dim rnd As New Random(DateTime.Now.Millisecond*2)
+			Dim list As New List(Of SimpleDataPoint)()
+			For i As Integer = 0 To 9
+				Dim x As Integer = i
+				Dim y As Double = (rnd.NextDouble() - 0.5) * 2
+				list.Add(New SimpleDataPoint(x, y))
+			Next i
+			Return list
+		End Function
+	End Class
+	Public Class SimpleDataPoint
+		Private privateX As Double
+		Public Property X() As Double
+			Get
+				Return privateX
+			End Get
+			Private Set(ByVal value As Double)
+				privateX = value
+			End Set
+		End Property
+		Private privateY As Double
+		Public Property Y() As Double
+			Get
+				Return privateY
+			End Get
+			Private Set(ByVal value As Double)
+				privateY = value
+			End Set
+		End Property
+		Public Sub New(ByVal arg As Double, ByVal val As Double)
+			X = arg
+			Y = val
+		End Sub
+	End Class
+End Namespace
